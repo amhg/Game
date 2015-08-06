79fb94ed833b73e2a96f9d7b582a1267ab93e558
amhg <andrea.cursos11@gmail.com>
2015-08-06 15:35:54 +0200

    comment added

--- src/andreaConnect4/logic.java
+++ src/andreaConnect4/logic.java
@@ -6,23 +6,14 @@ package andreaConnect4;
 
 
 public class logic {
-    private int cells_left = 0;
-    private int max;
-    private int xsize;
-    private int ysize;
-    Grid my_grid;
-    
-    public logic(Grid tempGrid){
-        max = 4;//connect 4 or n
-        my_grid = tempGrid;
-        cells_left = my_grid.get_cells_left();
-        xsize = my_grid.get_xsize();
-        ysize = my_grid.get_ysize();
-        }
-    
-    public void set_and_check(int x, int y, int player){//sets the found coordinate to current player
-        my_grid.set_matrix(x, y, player);
-        cells_left--;
-        }
+
+  
+   public void checkVertical(){}
+   
+   public void checkHorizontal(){}
+   
+   public void checkDiagonalDown(){}
+   
+   public void checkDiagonalUp(){}
     
 }
